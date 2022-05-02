818dcef64235531dbf09ab32602f8da6c77c0d0d
Shilpa Nair <ssnair@usc.edu>
2022-05-01 19:51:15 -0700

    filtering based on train/combined vocabs, text preprocessing fn

--- trial/asl_preprocess_trials.py
+++ trial/asl_preprocess_trials.py
@@ -13,6 +13,22 @@ from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
 from img2vec import write_pickle_file
 from time import time
+import contractions
+
+def preprocess_text(in_file, out_file, delimiter='\t'):
+    def preprocess(text):
+        text = contractions.fix(text.lower()).lower()
+        text = ' '.join([x for x in word_tokenize(text) if x.isalnum()])
+        return text
+    print('Starting text preprocessing...')
+    print('Reading data from', in_file)
+    data = pd.read_csv(in_file, delimiter=delimiter)
+    data['name'] = data['SENTENCE_NAME']
+    data['text'] = data['SENTENCE'].apply(preprocess)
+    data = data[ ['name', 'text'] ]
+    # data.to_csv(out_file, index=False)
+    print('Text preprocess completed')
+    return data
 
 def filter_data(in_path, out_path, batch_size, keep_set, data=None):
     def drop_cross(other, text): #for test, dev datasets
@@ -54,8 +70,21 @@ def filter_data(in_path, out_path, batch_size, keep_set, data=None):
         write_pickle_file(towri, batch)
     print('Filter Process Completed')
 
-def get_vocab(h2s_path, dataset):
-    data = pd.read_csv(os.path.join(h2s_path, dataset+'.csv'), delimiter='\t')
+def get_all_ids(path):
+    print('Reading data from', path)
+    in_files = sorted(os.listdir(path))
+    ids = set()
+    for fi in in_files:
+        with gzip.open(os.path.join(path, fi)) as f:
+            fi = pickle.load(f)
+        for d in fi:
+            ids.add(d['name'])
+    print('No of IDs found', len(ids))
+    return ids
+
+def get_vocab(h2s_path, dataset, data = None):
+    if type(data)==type(None):
+        data = pd.read_csv(os.path.join(h2s_path, dataset+'.csv'), delimiter='\t')
     data['WORDS'] = data['SENTENCE'].apply(lambda y: [x for x in word_tokenize(y.lower()) if x.lower() not in stopwords.words('english') and x.replace("'", '').isalnum()])
     # data['VOCAB'] = data['WORDS'].apply(set)
     sets = data['WORDS'].tolist()
@@ -95,7 +124,7 @@ def add_drop_column(vocab, data, thresh):
         return False
     drop_set = {x for x in vocab if vocab[x]<=thresh}
     data['DROPPED'] = data['WORDS'].apply(can_drop)
-    print('After dropping instances with words with freq <=',thresh,'dataset size:', len(data[~data['DROPPED']]), '/',len(data))
+    print('After dropping instances with words with freq <=',thresh,'dataset size:', len(data[~data['DROPPED']]), '/', len(data))
 
 def plot_vocab_counts(vocab, name):
     vocab = sorted(vocab.items(), key=lambda x: -x[-1])
@@ -120,24 +149,82 @@ def main():
     # print('For test ::', plot_vocab_counts(test, 'test.png'))
     # train = {x:train[x] for x in train if train[x]>200}
 
-    thresh = 2
+    thresh = 40
+    input_main_path = '/scratch2/maiyaupp/how2sign/how2sign_vitb16/'
+    print('START :: FILTERING BASED ON TRAIN VOCABULARY')
+    output_folder = 'filtered_thresh40'
+
     train, data = get_vocab(h2s_path, 'train')
     add_drop_column(train, data, thresh)
     batch_size = 100
     keep_set = {x for x in train if train[x]>thresh}
     print('Keeping data with word frequency above', thresh, 'and using batch size', batch_size)
-    in_path = '/scratch2/maiyaupp/how2sign/how2sign_vitb16/train'
-    out_path = '/scratch2/maiyaupp/how2sign/how2sign_vitb16/filtered_thresh2_att2/train'
+    in_path = input_main_path + 'train'
+    out_path = '/scratch2/maiyaupp/how2sign/how2sign_vitb16/'+output_folder+'/train'
     filter_data(in_path, out_path, batch_size, keep_set, data=data)
+    in_path = input_main_path + 'test'
+    out_path = '/scratch2/maiyaupp/how2sign/how2sign_vitb16/'+output_folder+'/test'
+    filter_data(in_path, out_path, batch_size, keep_set)
+    in_path = input_main_path + 'dev'
+    out_path = '/scratch2/maiyaupp/how2sign/how2sign_vitb16/'+output_folder+'/dev'
+    filter_data(in_path, out_path, batch_size, keep_set)
 
+    print('START :: FILTERING BASED ON COMBINED (TRAIN+DEV+TEST) VOCABULARY')
+    output_folder = 'filtered_mix_thresh40'
+
+    tr = pd.read_csv(os.path.join(h2s_path, 'train.csv'), delimiter='\t')
+    tr['DATASET'] = 'TRAIN'
+    te = pd.read_csv(os.path.join(h2s_path, 'test.csv'), delimiter='\t')
+    te['DATASET'] = 'TEST'
+    de = pd.read_csv(os.path.join(h2s_path, 'dev.csv'), delimiter='\t')
+    de['DATASET'] = 'DEV'
+    data = pd.concat([tr, te, de])
+    vocab, data = get_vocab(h2s_path, 'combined', data=data)
+    add_drop_column(vocab, data, thresh)
+    batch_size = 100
+    keep_set = {x for x in vocab if vocab[x]>thresh}
+    print('Keeping data with word frequency above', thresh, 'and using batch size', batch_size)
+    in_path = input_main_path + 'train'
+    out_path = '/scratch2/maiyaupp/how2sign/how2sign_vitb16/'+output_folder+'/train'
+    filter_data(in_path, out_path, batch_size, keep_set, data=data[data['DATASET']=='TRAIN'])
+    in_path = input_main_path + 'test'
+    out_path = '/scratch2/maiyaupp/how2sign/how2sign_vitb16/'+output_folder+'/test'
+    filter_data(in_path, out_path, batch_size, keep_set, data=data[data['DATASET']=='TEST'])
+    in_path = input_main_path + 'dev'
+    out_path = '/scratch2/maiyaupp/how2sign/how2sign_vitb16/'+output_folder+'/dev'
+    filter_data(in_path, out_path, batch_size, keep_set, data=data[data['DATASET']=='DEV'])
+
+    # ids = get_all_ids(out_path)
+    # sent_ids = set(data[~data['DROPPED']]['SENTENCE_NAME'].unique())
+    # f = open('stats.txt', 'w')
+    # temp = sent_ids - ids
+    # f.write('In csv, not in folder:\n\n')
+    # f.write( '\n'.join(temp) )
+    # f.close()
     # print('For train ::', plot_vocab_counts(train, 'train_nostp_200.png'))
+
+def main_text_preprocess():
+    out_file = '/scratch2/maiyaupp/how2sign/text_preprocessed/combined.csv'
+    
+    in_file = '/scratch2/maiyaupp/how2sign/dev.csv'
+    dev = preprocess_text(in_file, out_file, delimiter='\t')
+    
+    in_file = '/scratch2/maiyaupp/how2sign/test.csv'
+    test = preprocess_text(in_file, out_file, delimiter='\t')
+    
+    in_file = '/scratch2/maiyaupp/how2sign/train.csv'
+    train = preprocess_text(in_file, out_file, delimiter='\t')
+
+    combined = pd.concat([dev, test, train])
+    combined.to_csv(out_file, index=False)
 
 if __name__ == '__main__':
     start = time()
     # ap = argparse.ArgumentParser("DeepDive")
     # ap.add_argument("--datapath", help="Which dataset to run on")
     # args = ap.parse_args()
-    main()
+    # main()
+    main_text_preprocess()
     print('Completed in:', (time() - start))
 # For dev :: 3196
 # For test :: 3662
